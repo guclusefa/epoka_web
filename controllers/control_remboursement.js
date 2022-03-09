c608ce7c63a2b572d405e0b5ff3ea22d047f6ba0
Sefa GUCLU <sefaaguclu@gmail.com>
2022-03-09 23:21:54 +0100

    remboursement

--- controllers/control_remboursement.js
+++ controllers/control_remboursement.js
@@ -5,13 +5,21 @@ module.exports = {
     afficher: function (req, res) {
         if (req.session.user_info !== undefined) { // si connecte
             titre = "Remboursement des missions";
-            model_remboursement.lister(function (lesMissions, lesComA, lesComB) {
-                for (i in lesMissions) {
-                    lesMissions[i].comA_nom = lesComA[i].com_nom
-                    lesMissions[i].comB_nom = lesComB[i].com_nom
+            model_remboursement.lister(function (lesMissionsTotal, lesMissions, lesComA, lesComB) {
+                for (i in lesMissionsTotal) {
+                    lesMissionsTotal[i].comA_nom = lesComA[i].com_nom
+                    lesMissionsTotal[i].comB_nom = lesComB[i].com_nom
                 }
-                console.log(lesMissions)
-                res.render('./remboursement', { titre, valid: req.flash('valid'), erreur: req.flash('erreur'), user_info: req.session.user_info, lesMissions })
+
+                for (i in lesMissionsTotal) {
+                    for (j in lesMissions) {
+                        if (lesMissions[j].mis_id == lesMissionsTotal[i].mis_id) {
+                            lesMissionsTotal[i].dis_km = lesMissions[j].dis_km
+                        }
+                    }
+                }
+
+                res.render('./remboursement', { titre, valid: req.flash('valid'), erreur: req.flash('erreur'), user_info: req.session.user_info, lesMissions: lesMissionsTotal })
             })
         } else {
             res.redirect('./')
--- models/model_remboursement.js
+++ models/model_remboursement.js
@@ -2,25 +2,28 @@ var db = require("../config/database");
 module.exports = {
     // page d'accueil
     lister: function (callback) {
-        var sql =`SELECT *, DATEDIFF(mis_fin, mis_debut)+1 AS mis_jour, DATE_FORMAT(mis_debut, '%d/%m/%Y') as mis_debut, DATE_FORMAT(mis_fin, '%d/%m/%Y') as mis_fin FROM communes, salaries, agences, missions, distances WHERE com_id = age_idCom AND sal_idAgence = age_id AND mis_idSal = sal_id AND ((dis_idComA = mis_idSalCom AND dis_idComB = mis_idCom) OR (dis_idComA = mis_idCom AND dis_idComB = mis_idSalCom)) ORDER BY mis_id` 
-        var sqlComA =`SELECT * FROM missions, communes WHERE com_id = mis_idSalCom ORDER BY mis_id` 
-        var sqlComB =`SELECT * FROM missions, communes WHERE com_id = mis_idCom ORDER BY mis_id` 
-        db.query(sql, function (err, data) {
-            db.query(sqlComA, function (err, data2) {
-                db.query(sqlComB, function (err, data3) {
-                    if (err) throw err;
-                    return callback(data, data2, data3);
+        var sql0 = `SELECT *, DATEDIFF(mis_fin, mis_debut)+1 AS mis_jour, DATE_FORMAT(mis_debut, '%d/%m/%Y') as mis_debut, DATE_FORMAT(mis_fin, '%d/%m/%Y') as mis_fin FROM salaries, missions WHERE mis_idSal = sal_id AND mis_validee = 1 ORDER BY mis_id`
+        var sql1 = `SELECT *, DATEDIFF(mis_fin, mis_debut)+1 AS mis_jour, DATE_FORMAT(mis_debut, '%d/%m/%Y') as mis_debut, DATE_FORMAT(mis_fin, '%d/%m/%Y') as mis_fin FROM communes, salaries, agences, missions, distances WHERE com_id = age_idCom AND sal_idAgence = age_id AND mis_idSal = sal_id AND ((dis_idComA = mis_idSalCom AND dis_idComB = mis_idCom) OR (dis_idComA = mis_idCom AND dis_idComB = mis_idSalCom)) AND mis_validee = 1 ORDER BY mis_id`
+        var sqlComA = `SELECT * FROM missions, communes WHERE com_id = mis_idSalCom ORDER BY mis_id`
+        var sqlComB = `SELECT * FROM missions, communes WHERE com_id = mis_idCom ORDER BY mis_id`
+        db.query(sql0, function (err, data0) {
+            db.query(sql1, function (err, data1) {
+                db.query(sqlComA, function (err, data2) {
+                    db.query(sqlComB, function (err, data3) {
+                        if (err) throw err;
+                        return callback(data0, data1, data2, data3);
+                    });
                 });
             });
         });
     },
 
     rembourser: function (params, callback) {
-        var sql =`UPDATE missions
+        var sql = `UPDATE missions
         SET mis_payee = 1
-        WHERE mis_id = ?` 
-        db.query(sql, params, function(err,data){
-            if(err)throw err;
+        WHERE mis_id = ?`
+        db.query(sql, params, function (err, data) {
+            if (err) throw err;
             return callback(data);
         });
     },
