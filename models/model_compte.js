a99ac7434f2c556a5c9a559c3aaa5dfe88fff46d
GUCLU Sefa <sefaaguclu@gmail.com>
2022-03-16 09:23:21 +0100

    mon compte

--- models/model_compte.js
+++ models/model_compte.js
@@ -2,7 +2,7 @@ var db = require("../config/database");
 module.exports = {
     // page d'accueil
     ficher: function (params, callback) {
-        var sql = "SELECT * FROM salaries, agences WHERE sal_idAgence = age_id AND sal_id = ?";
+        var sql = "SELECT * FROM salaries, agences, communes WHERE sal_idAgence = age_id AND age_idCom = com_id AND sal_id = ?";
         db.query(sql, params, function (err, data) {
             if (err) throw err;
             return callback(data);
