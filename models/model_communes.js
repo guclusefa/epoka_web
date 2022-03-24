7e9cceeb9cbd17ec2e9c6aa6170b7957c6d18f6e
Sefa GUCLU <sefaaguclu@gmail.com>
2022-03-24 20:28:45 +0100

    remove limit

--- models/model_communes.js
+++ models/model_communes.js
@@ -2,7 +2,7 @@ var db = require("../config/database");
 module.exports = {
     // page d'accueil
     lister: function (callback) {
-        var sql = "SELECT * FROM communes LIMIT 500";
+        var sql = "SELECT * FROM communes";
         db.query(sql, function (err, data) {
             if (err) throw err;
             return callback(data);
@@ -17,6 +17,7 @@ module.exports = {
         });
     },
 
+    /* test */
     chercher: function (params, callback) {
         var sql = 'SELECT * FROM communes WHERE com_nom LIKE ?' + '"%"';
 
